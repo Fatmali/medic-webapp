ac49a070e838ec91be052abd1f698ced1707dc9d
Diana Barsan <barsan@medicmobile.org>
2018-11-06 17:22:47 +0200

    Fixes accept_patient_reports to clear offline schedules.

--- sentinel/src/transitions/accept_patient_reports.js
+++ sentinel/src/transitions/accept_patient_reports.js
@@ -127,16 +127,16 @@ const findValidRegistration = (doc, registrations) => {
             var nextTask = scheduled_tasks[j + 1] || { due: moment(visitReportedDate).add(1, 'd') };
             if (nextTask && moment(nextTask.due) > moment(visitReportedDate)) {
               // We loop through tasks. Once we find one that has either the status "delivered" or "sent" with
-              // a future task with a due date that is after the visit reported date then we have found the task 
+              // a future task with a due date that is after the visit reported date then we have found the task
               // linked to the visit. We always link if this is the last scheduled task delivered or sent.
               var taskIndex = _.findIndex(registration.scheduled_tasks, { due: task.due });
               registration.scheduled_tasks[taskIndex].report_uuid = doc._id;
               return registration;
             }
-          }          
+          }
         }
       }
-    } 
+    }
 };
 
 const addReportUUIDToRegistration = (doc, registrations, callback) => {
@@ -144,7 +144,7 @@ const addReportUUIDToRegistration = (doc, registrations, callback) => {
     if (validRegistration) {
       return db.medic.put(validRegistration, callback);
     }
-    
+
     callback();
 };
 
@@ -197,16 +197,9 @@ const addMessagesToDoc = (doc, config, registrations) => {
 };
 
 const handleReport = (doc, config, callback) => {
-  utils.getRegistrations(
-    {
-      db: dbNano,
-      id: doc.fields.patient_id,
-    },
-    (err, registrations) => {
-      if (err) {
-        return callback(err);
-      }
-
+  utils
+    .getReportsBySubject({ db: db.medic, ids: utils.getSubjectIds(doc.patient ), registrations: true })
+    .then(registrations => {
       addMessagesToDoc(doc, config, registrations);
       addRegistrationToDoc(doc, registrations);
       addReportUUIDToRegistration(doc, registrations, err => {
@@ -216,8 +209,8 @@ const handleReport = (doc, config, callback) => {
 
         module.exports.silenceRegistrations(config, doc, registrations, callback);
       });
-    }
-  );
+    })
+    .catch(err => callback(err));
 };
 
 module.exports = {
